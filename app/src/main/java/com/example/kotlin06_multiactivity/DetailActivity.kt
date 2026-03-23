2635e37b0155e14c869430398a9e86b3ed144afa
Joelinton Dorte <joelinton.dorte@ufpr.br>
2026-03-23 19:01:04 -0300

    Criacao segunda tela

--- app/src/main/java/com/example/kotlin06_multiactivity/DetailActivity.kt
+++ app/src/main/java/com/example/kotlin06_multiactivity/DetailActivity.kt
@@ -1,28 +1,27 @@
 package com.example.kotlin06_multiactivity
 
-import android.content.Intent
 import android.os.Bundle
-import android.view.View
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import kotlin.jvm.java
 
-class MainActivity : AppCompatActivity() {
+class DetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_detail)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-    }
 
-    fun goNextActivity(view: View) {
-        val intent = Intent(this, DetailActivity::class.java)
-        startActivity(intent)
+        val closeButton = findViewById<Button>(R.id.closeButton)
+        closeButton.setOnClickListener {
+            finish()
+        }
+
     }
 }
